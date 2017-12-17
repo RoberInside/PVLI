1e2ab7ad70a87778625ed6ee7864e01f289b2d7b
borjacano97 <borcano@ucm.es>
2017-12-17 17:48:25 +0100

    Abstraccion de patrones

--- src/js/Enemy.js
+++ src/js/Enemy.js
@@ -2,18 +2,21 @@ var Projectile = require('./Projectile.js')
 
 
 var Enemy = function(state, game, x, y){
-	Phaser.Sprite.call(this, game, 0, 0, 'enemy');
+	Phaser.Sprite.call(this, game, x, y, 'enemy');
 	this.game = game;
 	this.game.physics.enable(this);
 	this.state = state;
-	this.x = x;
-	this.y = y;
+	this.Pattern = undefined;
 	this.hasShot = false;
+
 };
 Enemy.prototype = Object.create (Phaser.Sprite.prototype);
 Enemy.prototype.constructor = Enemy;
 
-Enemy.prototype.move = function() {};
+Enemy.prototype.move = function() {
+	//TODO
+	console.log("Function move() from Enemy not implemented yet");
+};
 
 Enemy.prototype.kill = function() {
 	//TODO
@@ -24,28 +27,19 @@ Enemy.prototype.update = function () {
 };
 Enemy.prototype.shoot = function () {
 	if (!this.hasShot){
-	let scope = inRadians(90);
-	let initialAngle = (Math.PI - scope) /2;
-	let finalAngle = initialAngle + scope;
-	let nBullets = 3;
-	let incr = scope / nBullets;
-		for (var i = initialAngle; i < finalAngle; i+= incr)
-		{
-			let vx = Math.cos(i);
-			let vy = Math.sin(i);
-			this.createBullet(vx, vy);
+		this.hasShot = true;
+
+		if (this.Pattern === undefined){
+			this.Pattern = new Pattern(this, this.patternType, 90, 0.5, 50);
 		}
-	this.hasShot = true;
+		this.Pattern.tick();
 	}
 }
 
 Enemy.prototype.createBullet = function (vx ,vy) {
-	let bullet = new Projectile(this.state, this.game, this.x, this.y, vx*50, vy*50);
+	let bullet = new Projectile(this.state, this.game, this.x, this.y, vx, vy);
 		this.game.add.existing(bullet);
 		this.state.bullets.push(bullet);
 
-}
-function inRadians(degrees) {
-	return (degrees*Math.PI/180);
 }
 module.exports = Enemy;
